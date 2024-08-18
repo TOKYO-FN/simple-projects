15e7a37deba5202609aa028c957268ca77c60efd
TOKYO-FN <youssef.magdyii.12@gmail.com>
2024-08-18 15:52:40 +0300

    "Updated script.js: added console logs, set initial values for UI elements, and added event listeners for 'check' and 'again' buttons."

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+console.log((document.querySelector('.message').style.color = 'cyan'));
+
+document.querySelector('.number').textContent = 18;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  console.log('Button was clicked');
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  console.log('again was clicked');
+});
