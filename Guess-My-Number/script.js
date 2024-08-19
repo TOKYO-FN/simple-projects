36ba8b9cc350925d0feb5ed617357cbdc3236e44
TOKYO-FN <youssef.magdyii.12@gmail.com>
2024-08-19 13:13:58 +0300

    Simplified conditional statement for displaying 'Too high!' or 'Too low!' message

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -5,56 +5,6 @@
 // TODO 🎉 Correct number!
 // TODO score by2l b wa7d f kol mara
 // TODO awl ma bgyb el rakam s7 byb2a da el highscore
-// let sercretNumber = Math.round(Math.random() * 20) + 1;
-// let checkButton = document.querySelector('.check');
-// let againButton = document.querySelector('.again');
-// let valueInBox = document.querySelector('.guess');
-// let score = document.querySelector('.score');
-// let highscore = document.querySelector('.highscore');
-// let message = document.querySelector('.message');
-// let bodyElement = document.getElementsByTagName('BODY')[0];
-
-// let clickFunc = function () {
-//   let value = Number(valueInBox.value);
-//   if (!value) {
-//     message.textContent = '⛔ No Number';
-//   } else if (sercretNumber == value) {
-//     console.log('🎉 Correct number!');
-//     message.textContent = '🎉 Correct number!';
-//     if (Number(highscore.textContent) < Number(score.textContent))
-//       highscore.textContent = score.textContent;
-//     bodyElement.style.backgroundColor = 'green';
-//     return;
-//   } else if (value > sercretNumber) {
-//     message.textContent = '📉 Too high!';
-//     console.log('📉 Too high!');
-//   } else {
-//     message.textContent = '📈 Too low!';
-//     console.log('📈 Too low!');
-//   }
-//   score.textContent = Number(score.textContent) - 1;
-// };
-// let againFunc = function () {
-//   sercretNumber = Math.floor(Math.random() * 20) + 1;
-//   message.textContent = 'Start guessing...';
-//   score.textContent = 20;
-//   valueInBox.value = '';
-//   bodyElement.style.backgroundColor = '#222';
-//   console.log(sercretNumber);
-// };
-
-// console.log(sercretNumber);
-// document.addEventListener('keydown', function (event) {
-//   if (event.key === 'Enter') clickFunc();
-// });
-
-// document.addEventListener('keydown', function (event) {
-//   if (event.key === 'CapsLock') againFunc();
-// });
-// checkButton.addEventListener('click', clickFunc);
-
-// againButton.addEventListener('click', againFunc);
-
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
@@ -91,11 +41,8 @@ document.querySelector('.check').addEventListener('click', function () {
     } else {
       score--;
       document.querySelector('.score').textContent = score;
-      if (guess > secretNumber) {
-        document.querySelector('.message').textContent = '📈 Too high!';
-      } else {
-        document.querySelector('.message').textContent = '📉 Too low!';
-      }
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
     }
   }
 });
